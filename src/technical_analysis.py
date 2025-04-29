e9e87843f4c7318e7bd3078ebade98ab0ee349c0
damianecek <damian.parigal@gmail.com>
2025-04-29 16:26:39 +0200

    Making application ready for development

--- src/technical_analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import ta
-
-def add_technical_indicators(df):
-    df["SMA_50"] = ta.trend.sma_indicator(df["Close"], window=50)
-    df["RSI"] = ta.momentum.rsi(df["Close"], window=14)
-    df["MACD"] = ta.trend.macd(df["Close"])
-    return df
